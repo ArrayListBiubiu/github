f71d044fdbde5b889d82eb0910f0cbfeb7e00b5c
李正旭 <lizhengxu@huice.com>
2021-06-21 09:11:00 +0800

    dev-3.7 commit 444

--- src/Github.java
+++ src/Github.java
@@ -4,6 +4,7 @@ public class Github {
         System.out.println("master commit 111");
         System.out.println("master commit 222");
         System.out.println("master commit 333");
+        System.out.println("dev-3.7 commit 444");
 
 
 
