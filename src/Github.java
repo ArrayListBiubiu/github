50690eb983caa693eb2bc31347910a00ca4d7156
李正旭 <lizhengxu@huice.com>
2021-06-21 09:12:01 +0800

    dev-3.8 commit 555

--- src/Github.java
+++ src/Github.java
@@ -4,6 +4,7 @@ public class Github {
         System.out.println("master commit 111");
         System.out.println("master commit 222");
         System.out.println("master commit 333");
+        System.out.println("dev-3.8 commit 555");
 
 
 
